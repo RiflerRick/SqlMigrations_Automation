247fbfa3009ffaadc2539d2c43cb2e7b6552f228
RiflerRick <rajdeep.mukherjee@st.niituniversity.in>
2017-08-22 15:55:56 +0530

    added helloworld file

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+class Helloworld:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.name = ""
+
+    def get_name(self):
+        """
+
+        :return:
+        """
+        return "rajdeep"
+    def set_name(self, name):
+        """
+
+        :param name:
+        :return:
+        """
+        self.name = name
